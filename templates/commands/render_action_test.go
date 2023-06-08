c087ec5e82fc5e332506c627e7ea79cd2031bb5d
Dave Revell <revell@google.com>
2023-06-08 15:23:24 -0700

    lint

--- templates/commands/render_action_test.go
+++ templates/commands/render_action_test.go
@@ -1,3 +1,17 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package commands
 
 import (
@@ -40,8 +54,8 @@ func TestWalkAndModify(t *testing.T) {
 			name:            "multiple_replacements_should_work",
 			visitor:         fooToBarVisitor,
 			relPath:         "my_file.txt",
-			initialContents: map[string]string{"my_file.txt": "foo foo"},
-			want:            map[string]string{"my_file.txt": "bar bar"},
+			initialContents: map[string]string{"my_file.txt": "foo foo"}, //nolint:dupword
+			want:            map[string]string{"my_file.txt": "bar bar"}, //nolint:dupword
 		},
 		{
 			name:            "dot_dir_should_work",
