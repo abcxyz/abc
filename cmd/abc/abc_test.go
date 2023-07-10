6f8a30747a384603be43958b8b5453cefcb4fab1
Dave Revell <revell@google.com>
2023-07-10 15:47:54 -0700

    Add tests for abc.go (#93)
    Also fix a place where os.Stdout was hardcoded into render.go. It should
    use the output stream provided by the cli package for testability.

--- cmd/abc/abc_test.go
+++ cmd/abc/abc_test.go
@@ -14,9 +14,80 @@
 
 package main
 
-import "testing"
+import (
+	"context"
+	"runtime"
+	"strings"
+	"testing"
 
-func TestMain(t *testing.T) {
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/exp/slices"
+)
+
+func TestRootCmd(t *testing.T) {
 	t.Parallel()
-	t.Skipf("TODO")
+
+	cases := []struct {
+		name       string
+		args       []string
+		wantStdout string
+		wantStderr string
+		onlyGOOSes []string // error messages differ between platforms, use separate subtests
+		wantErr    string
+	}{
+		{
+			name:       "render_prints_to_stdout",
+			args:       []string{"templates", "render", "--input=person_name=Bob", "../../examples/templates/render/print"},
+			wantStdout: "Hello, Bob!\n",
+		},
+		{
+			name:       "error_return_non_windows",
+			args:       []string{"templates", "render", "nonexistent/dir"},
+			onlyGOOSes: []string{"linux", "darwin"},
+			wantErr:    "no such file or directory",
+		},
+		{
+			name:       "error_return_windows",
+			args:       []string{"templates", "render", "nonexistent/dir"},
+			onlyGOOSes: []string{"windows"},
+			wantErr:    "cannot find the path",
+		},
+		{
+			name:       "help_text",
+			args:       []string{"-h"},
+			wantStderr: "Usage: abc",
+		},
+		{
+			name:    "nonexistent_subcommand",
+			args:    []string{"nonexistent"},
+			wantErr: `unknown command "nonexistent": run "abc -help" for a list of commands`,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			if len(tc.onlyGOOSes) > 0 && !slices.Contains(tc.onlyGOOSes, runtime.GOOS) {
+				t.Skipf("this subtest only runs if GOOS is one of %s", tc.onlyGOOSes)
+			}
+
+			ctx := context.Background()
+			rc := rootCmd()
+			_, stdout, stderr := rc.Pipe()
+			err := rc.Run(ctx, tc.args)
+			if diff := testutil.DiffErrString(err, tc.wantErr); diff != "" {
+				t.Error(diff)
+			}
+			if !strings.Contains(stdout.String(), tc.wantStdout) {
+				t.Errorf("stdout was not as expected (-got,+want):\n%s", cmp.Diff(stdout.String(), tc.wantStdout))
+			}
+			if !strings.Contains(stderr.String(), tc.wantStderr) {
+				t.Errorf("stderr was not as expected (-got,+want):\n%s", cmp.Diff(stderr.String(), tc.wantStderr))
+			}
+		})
+	}
 }
