8e9228d0f021635e4bd3625978014b4fb4ee1ba3
pdewilde <pdewilde@google.com>
2023-08-07 13:11:46 -0700

    Revert "Throw something at the wall and see if it sticks"
    This reverts commit f4dde55c997e79538c47a0a4167351e3fe73a4d4.

--- templates/commands/render_action_append_test.go
+++ templates/commands/render_action_append_test.go
@@ -22,7 +22,6 @@ import (
 	"github.com/abcxyz/abc/templates/model"
 	"github.com/abcxyz/pkg/testutil"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestActionAppend(t *testing.T) {
@@ -192,7 +191,7 @@ func TestActionAppend(t *testing.T) {
 			}
 
 			got := loadDirWithoutMode(t, scratchDir)
-			if diff := cmp.Diff(got, tc.want, cmpFileMode, cmpopts.EquateEmpty()); diff != "" {
+			if diff := cmp.Diff(got, tc.want); diff != "" {
 				t.Errorf("scratch directory contents were not as expected (-got,+want): %v", diff)
 			}
 		})
