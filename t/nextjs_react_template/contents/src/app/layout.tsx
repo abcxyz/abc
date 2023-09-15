8f0c9edb7a72a9230d0db1ccbf600def68081140
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-09-15 08:43:47 +0000

    Add NavBar

--- t/nextjs_react_template/contents/src/app/layout.tsx
+++ t/nextjs_react_template/contents/src/app/layout.tsx
@@ -1,7 +1,8 @@
+import './global.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import Auth from './auth';
+import NavBar from './navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,8 +15,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <UserProvider>
-        <body className={inter.className}>
-          <Auth />
+        <body className={inter.className} >
+          <NavBar />
           {children}
         </body>
       </UserProvider>
---
+++ t/nextjs_react_template/contents/src/app/navbar.tsx
@@ -0,0 +1,21 @@
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import Auth from "./auth";
+import { APP_TITLE } from "./constants";
+
+export default function NavBar() {
+  return (
+    <AppBar position="static" sx={{ bgcolor: "#202124" }}>
+      <Toolbar>
+        <Typography
+          variant="h5"
+          component="div"
+          fontWeight={700}
+          sx={{ flexGrow: 1 }}
+        >
+          {APP_TITLE}
+        </Typography>
+        <Auth />
+      </Toolbar>
+    </AppBar>
+  )
+}
