5fdb1f3853d3adbd0d463c08e28964ba893e14c9
Kevin Yang <kevya@google.com>
2023-09-15 20:38:18 +0000

    try %q

--- templates/commands/render/action_include_test.go
+++ templates/commands/render/action_include_test.go
@@ -83,7 +83,7 @@ func TestActionInclude(t *testing.T) {
 					},
 				},
 			},
-			wantErr: fmt.Sprintf("path \"%s\" must not contain \"..\"", filepath.Clean("../file.txt")),
+			wantErr: fmt.Sprintf("path %q must not contain \"..\"", filepath.FromSlash("../file.txt")),
 		},
 		{
 			name: "templated_filename_success",
