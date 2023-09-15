65769ed78f5b5aefe2ee99db76447d1b56fc4b81
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-09-15 16:05:31 +0000

    Remove unused import

--- t/nextjs_react_template/contents/src/app/navbar.tsx
+++ t/nextjs_react_template/contents/src/app/navbar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import Auth from "./auth";
 import { APP_TITLE } from "./constants";
 
