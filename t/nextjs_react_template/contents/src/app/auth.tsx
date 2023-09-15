7fb625c7a3fb368b9ea37980167f2eb1a36e1a42
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-09-15 07:57:51 +0000

    Replace with mui components

--- t/nextjs_react_template/contents/src/app/auth.tsx
+++ t/nextjs_react_template/contents/src/app/auth.tsx
@@ -1,21 +1,21 @@
 'use client';
 
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { AUTH_LOADING_TEXT, AUTH_LOGIN_TEXT, AUTH_LOGOUT_TEXT } from './constants';
 
 export default function Auth() {
   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>{AUTH_LOADING_TEXT}</div>;
-  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <Box>{AUTH_LOADING_TEXT}</Box>;
+  if (error) return <Box>{error.message}</Box>;
 
   if (user) {
     return (
-      <div>
-        Welcome {user.email}!
+      <Box>
+        <Typography variant="h5">Welcome {user.email}!</Typography>
         <Button color="inherit" href="/api/auth/logout">{AUTH_LOGOUT_TEXT}</Button>
-      </div>
+      </Box>
     )
   }
 
