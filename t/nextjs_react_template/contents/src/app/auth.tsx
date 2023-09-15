15906403b31ae56ebda70f55c9195f544624310d
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-09-15 00:35:29 +0000

    Add constants file

--- t/nextjs_react_template/contents/src/app/auth.tsx
+++ t/nextjs_react_template/contents/src/app/auth.tsx
@@ -2,21 +2,22 @@
 
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { Button } from '@mui/material';
+import { AUTH_LOADING_TEXT, AUTH_LOGIN_TEXT, AUTH_LOGOUT_TEXT } from './constants';
 
 export default function Auth() {
   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>{AUTH_LOADING_TEXT}</div>;
   if (error) return <div>{error.message}</div>;
 
   if (user) {
     return (
       <div>
         Welcome {user.email}!
-        <Button color="inherit" href="/api/auth/logout">Logout</Button>
+        <Button color="inherit" href="/api/auth/logout">{AUTH_LOGOUT_TEXT}</Button>
       </div>
     )
   }
 
-  return <Button color="inherit" href="/api/auth/login">Login</Button>;
+  return <Button color="inherit" href="/api/auth/login">{AUTH_LOGIN_TEXT}</Button>;
 }
---
+++ t/nextjs_react_template/contents/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const AUTH_LOADING_TEXT = "Loading..."
+export const AUTH_LOGIN_TEXT = "Login"
+export const AUTH_LOGOUT_TEXT = "Log out"
+
