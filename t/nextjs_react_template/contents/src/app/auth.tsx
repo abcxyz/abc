b554ed944d7944dc5db88df3d016426a2e64f27c
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-09-15 13:05:01 -0700

    feat: add Auth0 UI components to nextjs_react_template (#212)
    This adds a login/logout flow to a configured Auth0 tenant and displays
    the user email when authenticated.
    Configuration of the Auth0 tenant through CLI inputs will be added in
    following PR.

---
+++ t/nextjs_react_template/contents/src/app/auth.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { Avatar, Box, Button, IconButton, Link, Menu, MenuItem } from '@mui/material';
+import { AUTH_LOADING_TEXT, AUTH_LOGIN_TEXT, AUTH_LOGOUT_TEXT } from './constants';
+import { useState } from 'react';
+
+export default function Auth() {
+  const { user, error, isLoading } = useUser();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  if (isLoading) {
+    return (
+      <Box>{AUTH_LOADING_TEXT}</Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box>{error.message}</Box>
+    );
+  }
+
+  if (user) {
+    return (
+      <Box sx={{ flexGrow: 0 }}>
+        <IconButton sx={{ p: 0 }}
+          onClick={handleMenu}
+        >
+          <Avatar alt="User Picture" src={user.picture ?? ''} />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem component={Link} href="/api/auth/logout">
+            {AUTH_LOGOUT_TEXT}
+          </MenuItem>
+        </Menu>
+      </Box>
+    )
+  }
+
+  return <Button color="inherit" href="/api/auth/login">{AUTH_LOGIN_TEXT}</Button>;
+}
---
+++ t/nextjs_react_template/contents/src/app/navbar.tsx
@@ -0,0 +1,21 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import Auth from "./auth";
+import { APP_TITLE } from "./constants";
+
+export default function NavBar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography
+          variant="h5"
+          component="div"
+          fontWeight="bold"
+          sx={{ flexGrow: 1 }}
+        >
+          {APP_TITLE}
+        </Typography>
+        <Auth />
+      </Toolbar>
+    </AppBar>
+  );
+}
