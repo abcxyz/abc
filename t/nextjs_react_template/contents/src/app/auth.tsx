875f01bc5b349696c731f6963b509dde8048197a
Rachel Cao <109982356+rachelcao-g@users.noreply.github.com>
2023-09-21 10:30:10 -0700

    check in csp to nextjs template (#217)

--- t/nextjs_react_template/contents/src/app/auth.tsx
+++ t/nextjs_react_template/contents/src/app/auth.tsx
@@ -1,12 +1,24 @@
 'use client';
 
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { Avatar, Box, Button, IconButton, Link, Menu, MenuItem } from '@mui/material';
-import { AUTH_LOADING_TEXT, AUTH_LOGIN_TEXT, AUTH_LOGOUT_TEXT } from './constants';
-import { useState } from 'react';
+import {useUser} from '@auth0/nextjs-auth0/client';
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import {
+  AUTH_LOADING_TEXT,
+  AUTH_LOGIN_TEXT,
+  AUTH_LOGOUT_TEXT,
+} from './constants';
+import {useState} from 'react';
 
 export default function Auth() {
-  const { user, error, isLoading } = useUser();
+  const {user, error, isLoading} = useUser();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -18,23 +30,17 @@ export default function Auth() {
   };
 
   if (isLoading) {
-    return (
-      <Box>{AUTH_LOADING_TEXT}</Box>
-    );
+    return <Box>{AUTH_LOADING_TEXT}</Box>;
   }
 
   if (error) {
-    return (
-      <Box>{error.message}</Box>
-    );
+    return <Box>{error.message}</Box>;
   }
 
   if (user) {
     return (
-      <Box sx={{ flexGrow: 0 }}>
-        <IconButton sx={{ p: 0 }}
-          onClick={handleMenu}
-        >
+      <Box sx={{flexGrow: 0}}>
+        <IconButton sx={{p: 0}} onClick={handleMenu}>
           <Avatar alt="User Picture" src={user.picture ?? ''} />
         </IconButton>
         <Menu
@@ -56,8 +62,12 @@ export default function Auth() {
           </MenuItem>
         </Menu>
       </Box>
-    )
+    );
   }
 
-  return <Button color="inherit" href="/api/auth/login">{AUTH_LOGIN_TEXT}</Button>;
+  return (
+    <Button color="inherit" href="/api/auth/login">
+      {AUTH_LOGIN_TEXT}
+    </Button>
+  );
 }
