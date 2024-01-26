f6250399729585705224086de6d5a1b802789cda
Dave Revell <revell@google.com>
2024-01-26 15:03:52 -0800

    Add require_golden_tests script, run in CI

---
+++ require_golden_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is intended to be invoked in a github action. It will return a
+# nonzero exit code and print a message if there are any example templates that
+# don't have a golden test. This will make sure we don't break any example
+# templates without realizing it.
+
+set -eEuo pipefail
+
+exit_status=0
+for spec_path in $(find . -name spec.yaml) ; do
+     template_dir=$(dirname $spec_path)
+     if [ ! -d "$template_dir/testdata/golden" ] ; then
+        echo "::error title=Missing golden test for template::$template_dir"
+        exit_status=1
+     fi
+done
+exit $exit_status
