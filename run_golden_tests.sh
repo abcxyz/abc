b54c701de0170da01d303e73b8481cff51a2bb0f
Dave Revell <revell@google.com>
2024-01-26 15:10:37 -0800

    Factor out run_golden_tests.sh so anyone can run it

---
+++ run_golden_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is intended to be invoked in a github action. It will run all the
+# the golden tests that it can find.
+
+set -eEuo pipefail
+
+exit_status=0
+for template_dir in $(find . -name 'testdata' | grep -v '/.git/' | xargs dirname) ; do
+go run cmd/abc/abc.go templates golden-test verify $template_dir
+if [[ $? != "0" ]]; then
+    exit_status=1
+    echo "::error title=Golden test failed::$template_dir"
+fi
+done
+exit $exit_status
