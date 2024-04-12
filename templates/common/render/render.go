e493b312c9527e582315935bf21aab16b90b066e
Dave Revell <revell@google.com>
2024-04-12 16:58:24 -0700

    Store fromDir in includedFromDest

--- templates/common/render/render.go
+++ templates/common/render/render.go
@@ -232,7 +232,7 @@ func RenderAlreadyDownloaded(ctx context.Context, dlMeta *templatesource.Downloa
 	sp := &stepParams{
 		debugDiffsDir:    debugStepDiffsDir,
 		ignorePatterns:   spec.Ignore,
-		includedFromDest: make(map[string]struct{}),
+		includedFromDest: make(map[string]string),
 		extraPrintVars:   extraPrintVars,
 		features:         spec.Features,
 		rp:               p,
@@ -401,7 +401,8 @@ type stepParams struct {
 	// These are paths relative to the --dest directory (which is the same thing
 	// as being relative to the scratch directory, the paths within these dirs
 	// are the same).
-	includedFromDest map[string]struct{}
+	// TODO update doc
+	includedFromDest map[string]string
 
 	// scope contains all variable names that are in scope. This includes
 	// user-provided scope, as well as any programmatically created variables
@@ -540,7 +541,7 @@ type commitParams struct {
 	dlMeta           *templatesource.DownloadMetadata
 	scratchDir       string
 	templateDir      string
-	includedFromDest map[string]struct{}
+	includedFromDest map[string]string
 	inputs           map[string]string
 }
 
@@ -602,7 +603,7 @@ func commitTentatively(ctx context.Context, p *Params, cp *commitParams) error {
 // The return value is a map containing a SHA256 hash of each file in
 // scratchDir. The keys are paths relative to scratchDir, using forward slashes
 // regardless of the OS.
-func commit(ctx context.Context, dryRun bool, p *Params, scratchDir string, includedFromDest map[string]struct{}) (map[string][]byte, error) {
+func commit(ctx context.Context, dryRun bool, p *Params, scratchDir string, includedFromDest map[string]string) (map[string][]byte, error) {
 	logger := logging.FromContext(ctx).With("logger", "commit")
 
 	if !dryRun {
