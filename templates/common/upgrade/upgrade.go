037a4234e29fdd814a6e881c208437718fdf0f11
Dave Revell <revell@google.com>
2024-04-16 18:21:35 -0700

    Improve the Result returned from the upgrade operation
     - Improve the Result returned by the Upgrade() library function by:
        - Changing the boolean status to an enum status because there are
          four distinct success/failure cases
        - Add a field for patch reversal conflicts, coming in a future PR
     - Add the ability for a command to return an exit code other than 0 or
       1. This is important because when an upgrade fails, I want to
          indicate why in the status code. This allows automation to detect
          what went wrong without resorting to parsing error messages.
        - Use this feature to return 2 for merge conflicts and 3 for patch
          reversal conflicts.

--- templates/common/upgrade/upgrade.go
+++ templates/common/upgrade/upgrade.go
@@ -107,11 +107,38 @@ type Params struct {
 	TempDirBase string
 }
 
+type ResultType string
+
+const (
+	// This is the value of Result.Type when the upgrade was vacuously
+	// successful because the template is already on the latest version.
+	AlreadyUpToDate ResultType = "already_up_to_date"
+
+	// This is the value of Result.Type when there was an upgrade and it was
+	// successful, and no user intervention is needed.
+	Success ResultType = "success"
+
+	// This is the value of Result.Type when abc tried to apply the reversal
+	// patches from the manifest to included-from-destination files, and the
+	// patches could not be applied cleanly. User intervention is needed, and
+	// the ReversalConflicts field should be used.
+	PatchReversalConflict ResultType = "patch_reversal_conflict"
+
+	// The new version of the template conflicted with local modifications and
+	// manual resolution is required. The Conflicts field should be used.
+	MergeConflict ResultType = "merge_conflict"
+)
+
 type Result struct {
 	// If no upgrade was done because this installation of the template is
 	// already on the latest version, then this will be true and all other
 	// fields in this struct will have zero values.
-	AlreadyUpToDate bool
+	Type ResultType
+
+	// The paths to files where abc tried to apply the reversal
+	// patches from the manifest to included-from-destination files, and the
+	// patches could not be applied cleanly. Manual resolution is needed.
+	ReversalConflicts []*ReversalConflict
 
 	// Conflicts is the set of files that require manual intervention by the
 	// user to resolve a merge conflict. For example, there may be a file that
@@ -125,6 +152,14 @@ type Result struct {
 	// This is mutually exclusive with "Conflicts". Each file is in only one of
 	// the two lists.
 	NonConflicts []ActionTaken
+}
+
+// ReversalConflict happens when abc tried to apply the reversal
+// patches from the manifest to included-from-destination files, and the patches
+// could not be applied cleanly.
+type ReversalConflict struct {
+	// The relative path to the file that needs manual resolution.
+	Path string
 }
 
 // ActionTaken represents an output of the merge operation. Every file that's
@@ -226,7 +261,7 @@ func Upgrade(ctx context.Context, p *Params) (_ *Result, rErr error) {
 	}
 	if hashMatch {
 		// No need to upgrade. We already have the latest template version.
-		return &Result{AlreadyUpToDate: true}, nil
+		return &Result{Type: AlreadyUpToDate}, nil
 	}
 
 	// The "merge directory" is yet another temp directory in addition to
@@ -282,7 +317,12 @@ func Upgrade(ctx context.Context, p *Params) (_ *Result, rErr error) {
 
 	conflicts, nonConflicts := partitionConflicts(actionsTaken)
 
+	resultType := MergeConflict
+	if len(conflicts) == 0 {
+		resultType = Success
+	}
 	return &Result{
+		Type:         resultType,
 		Conflicts:    conflicts,
 		NonConflicts: nonConflicts,
 	}, nil
